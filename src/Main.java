8cb08c706d8044c6d2f4be4049edf02d60b952af
ZubinSundaram <zubins2@nycstudents.net>
2025-10-17 15:35:45 -0400

    finishing; completed 2 methods.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args)
+    {
+        WordMatch game1 = new WordMatch("mississippi");
+        System.out.println(game1.scoreGuess("i"));
+        System.out.println(game1.scoreGuess("iss"));
+        System.out.println(game1.scoreGuess("issipp"));
+        System.out.println(game1.scoreGuess("mississippi"));
+
+        WordMatch game2 = new WordMatch("concatenation");
+        System.out.println(game2.scoreGuess("ten"));
+        System.out.println(game2.scoreGuess("nation"));
+        System.out.println(game2.findBetterGuess("ten", "nation"));
+    }
 }
--- src/WordMatch.java
+++ src/WordMatch.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+public class WordMatch
+{
+    /** The secret string. */
+    private String secret;
 
-public class WordMatch {
+    /** Constructs a WordMatch object with the given secret string of lowercase letters. */
+    public WordMatch(String word)
+    {
+        secret = word;
+    }
+
+    /** Returns a score for guess, as described in part (a)
+     * Precondition: 0 < guess.length() <= secret.length()
+     */
+    public int scoreGuess(String guess)
+    {
+        int occurrences = 0;
+        for (int i = 0; i <= secret.length() - guess.length(); i++)
+        {
+            if (secret.substring(i, i+guess.length()).equals(guess)) occurrences++;
+        }
+        return occurrences*guess.length()*guess.length();
+    }
+
+    /** Returns the better of two guesses, as determined by scoreGuess and the rules
+     * for a tie-breaker that are described in part (b).
+     * Precondition: guess1 and guess2 contain all lowercase letters.
+     *               guess1 is not the same as guess2.
+     */
+    public String findBetterGuess(String guess1, String guess2)
+    {
+        if(scoreGuess(guess1) > scoreGuess(guess2)) return guess1;
+        else if(scoreGuess(guess1) < scoreGuess(guess2)) return guess2;
+        else if(guess1.compareTo(guess2) > 0) return guess1;
+        else return guess2;
+    }
 }
